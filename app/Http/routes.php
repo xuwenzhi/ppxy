b84d14bf7cb26805555eb94e56e792809929d646
xuwenzhi <358350782@qq.com>
2015-05-18 16:11:22 +0800

    update routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+Route::get('home', 'Home\IndexController@index');
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
