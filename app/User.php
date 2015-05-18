225f3be85af1ec971da60edbcc3bd4fcf626087e
xuwenzhi <358350782@qq.com>
2015-05-18 14:43:25 +0800

    fix auth default user table

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var string
 	 */
-	protected $table = 'user';
+	protected $table = 'users';
 
 	/**
 	 * The attributes that are mass assignable.
