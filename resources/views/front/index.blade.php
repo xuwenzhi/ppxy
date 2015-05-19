1a44e20b581788c7dec5d69f8f88b52831c159ce
xuwenzhi <358350782@qq.com>
2015-05-19 10:15:56 +0800

    update index

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -7,52 +7,7 @@
 			<div class="panel panel-default">
 				<div class="panel-heading">登录</div>
 				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">邮件地址</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">密码</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> 记住我?
-									</label>
-								</div>
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">登录</button>
-
-								<a class="btn btn-link" href="{{ url('/password/email') }}">忘记密码?</a>
-							</div>
-						</div>
-					</form>
+					首页
 				</div>
 			</div>
 		</div>
