25296000c2a357ccfadd8765e57857ab6a6aaa59
xuwenzhi <358350782@qq.com>
2015-05-21 00:26:55 +0800

    no scale

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>@yield('title') - PP校园</title>
-
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
 	<link href="{{ asset('/css/bootstrap.min.css') }}" rel="stylesheet" />
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
