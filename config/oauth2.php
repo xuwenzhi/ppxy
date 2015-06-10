4979a4279d74b35d1f6e25b012dcdb26ad11e271
xuwenzhi <358350782@qq.com>
2015-06-10 11:29:35 +0800

    gitignore

--- config/oauth2.php
+++ config/oauth2.php
@@ -32,21 +32,20 @@ return [
     */
 
     'grant_types' => [
-	'password' => [
-        'class' => '\League\OAuth2\Server\Grant\PasswordGrant',
-        'callback' => function($username, $password) {
-            if( Auth::validate([
-                'email'    => $username,
-                'password' => $password,
-            ])){
-                  $user = \App\User::where('email',$username)->first();
-                  return $user->id;
-              } else {
-                  return false;
-              }
-    	    },
-        'access_token_ttl' => 3600
-    ]
+    	'password' => [
+            'class' => '\League\OAuth2\Server\Grant\PasswordGrant',
+            'callback' => function($username, $password) {
+                if( Auth::validate([
+                    'email'    => $username,
+                    'password' => $password,
+                ])){
+                      $user = \App\User::where('email',$username)->first();
+                      return $user->id;
+                  } else {
+                      return false;
+                  }
+            }
+        ]
     ],
     /*
     |--------------------------------------------------------------------------
