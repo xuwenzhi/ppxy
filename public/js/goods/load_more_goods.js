87df632126f95a51dd2a3be6fa885df668b5a718
xuwenzhi <358350782@qq.com>
2015-09-09 22:51:46 +0800

    fix

--- public/js/goods/load_more_goods.js
+++ public/js/goods/load_more_goods.js
@@ -76,13 +76,13 @@ function load_more_goods($type, $page){
 	            	needle.push($page, $page);
 	            	$(window).on('load', function(){});
 	            	$("#page").attr("data-type", $page+1);
-	            	var t = $(window).scrollTop();
-					$('body').animate({'scrollTop':t+240},1300);
+	            	//var t = $(window).scrollTop();
+					//$('body').animate({'scrollTop':t+240},1300);
 				}else{
 					canLoad = false;
 					$("#load_res_txt").show();
-					var t = $(window).scrollTop();
-					$('body').animate({'scrollTop':t+200},1300);
+					//var t = $(window).scrollTop();
+					//$('body').animate({'scrollTop':t+200},1300);
 				}
 				$("div[id='load']").hide();
 			} else if(data.status == 'error'){
