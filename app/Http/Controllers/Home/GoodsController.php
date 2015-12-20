3e182406a964d982a25c258dcc2fb8c07cc65877
xuwenzhi <358350782@qq.com>
2015-12-20 15:18:20 +0800

    Fix xss

--- app/Http/Controllers/Home/GoodsController.php
+++ app/Http/Controllers/Home/GoodsController.php
@@ -80,10 +80,10 @@ class GoodsController extends HomeController {
 	 */
 	private function _insert($arrData){
 		$objGoods = new Goods;
-		$objGoods->title = $arrData['goods_title'];
+		$objGoods->title = htmlspecialchars($arrData['goods_title']);
 		$objGoods->type = $arrData['goods_type'];
 		$objGoods->price = $arrData['goods_price'];
-		$objGoods->content = trim($arrData['goods_content']);
+		$objGoods->content = htmlspecialchars(trim($arrData['goods_content']));
 		$objGoods->uid = $arrData['uid'];
 		$objGoods->special = Goods::SPECIAL_NORMAL;
 		$objGoods->status  = Goods::STATUS_SELL;
